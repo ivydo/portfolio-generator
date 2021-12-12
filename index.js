8f71d3c9c70cad3555521bd2b9493df2486ebfe2
Ivy Do <ivy.do2011@gmail.com>
2021-12-11 21:47:57 -0500

    in progress

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+  .prompt([
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name> (Required)',
+          validate: nameInput => {
+              return true;
+          }else {
+              return 'Please enter your name';
+          }
+          }
+      },
+  ])
